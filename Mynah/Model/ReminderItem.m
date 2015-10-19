8e4d5a570db8a320c56f6d1f49f572fc3994b469
Joshua Marker <jmarker@usertesting.com>
2015-10-19 13:37:46 -0700

    Add NSCoder protocol for saving

--- Mynah/Model/ReminderItem.m
+++ Mynah/Model/ReminderItem.m
@@ -1,5 +1,23 @@
 #import "ReminderItem.h"
 
+@interface ReminderItem () <NSCoding>
+@end
+
 @implementation ReminderItem
+
+#pragma mark - NSCoder Protocol
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:self.reminderTitle forKey:@"reminderTitle"];
+    [coder encodeObject:self.reminderText forKey:@"reminderText"];
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super init];
+    if (self) {
+        self.reminderTitle = [coder decodeObjectForKey:@"reminderTitle"];
+        self.reminderText = [coder decodeObjectForKey:@"reminderText"];
+    }
+    return self;
+}
 
 @end
--- Mynah/Model/ReminderList.m
+++ Mynah/Model/ReminderList.m
@@ -1,6 +1,6 @@
 #import "ReminderList.h"
 
-@interface ReminderList ()
+@interface ReminderList () <NSCoding>
 
 @property (strong, nonatomic, readwrite) NSMutableArray *privateReminders;
 
@@ -18,6 +18,23 @@
 
 - (NSArray *)reminders {
     return [NSArray arrayWithArray:self.privateReminders];
+}
+
+#pragma mark - NSCoder Protocol
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:self.privateReminders forKey:@"privateReminders"];
+    [coder encodeObject:self.listName forKey:@"listName"];
+    [coder encodeFloat:[self.frequency floatValue] forKey:@"frequency"];
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super init];
+    if (self) {
+        self.privateReminders = [coder decodeObjectForKey:@"privateReminders"];
+        self.listName = [coder decodeObjectForKey:@"listName"];
+        self.frequency = @([coder decodeFloatForKey:@"frequency"]);
+    }
+    return self;
 }
 
 @end
