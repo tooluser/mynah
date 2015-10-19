bd05b7a65c51596bf4ec248f37c4169cc653eda3
Joshua Marker <jmarker@usertesting.com>
2015-10-19 13:07:46 -0700

    Initial Commit

---
+++ Mynah/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mynah
+//
+//  Created by Joshua Marker on 10/19/15.
+//  Copyright © 2015 Nowhereville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
