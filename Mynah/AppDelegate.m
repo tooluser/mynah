85598e0bd75c5417c0cf1b24d87bd26fbe657e42
Joshua Marker <jmarker@usertesting.com>
2015-11-02 13:06:47 -0800

    Create ReminderList object and set it on the ReminderListViewConrtroller

--- Mynah/AppDelegate.m
+++ Mynah/AppDelegate.m
@@ -1,12 +1,17 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 #import "ReminderListViewController.h"
+#import "ReminderList.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     UIWindow *window = [[UIWindow alloc] init];
-    window.rootViewController = [[ReminderListViewController alloc] init];
+
+    ReminderListViewController *reminderListViewController = [[ReminderListViewController alloc] init];
+
+    reminderListViewController.reminderList = [ReminderList new];
+    window.rootViewController = reminderListViewController;
     [window makeKeyAndVisible];
     self.window = window;
     
